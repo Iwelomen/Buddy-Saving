f0b3c995accfa8b3254b99481d14852f1ae813a2
Godspower Obami <godspowerobami@gmail.com>
2023-02-03 16:09:20 +0100

    complete screen 5 UI design

--- lib/screens/screen5.dart
+++ lib/screens/screen5.dart
@@ -1,6 +1,8 @@
 import 'package:buddy_saving/Utils/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../Utils/app_button.dart';
+
 class ScreenFive extends StatefulWidget {
   const ScreenFive({Key? key}) : super(key: key);
 
@@ -9,15 +11,296 @@ class ScreenFive extends StatefulWidget {
 }
 
 class _ScreenFiveState extends State<ScreenFive> {
+  var _on = false;
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              CustomAppBar(),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomAppBar(),
+                const Text(
+                  'Invite your buddy',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                    'An invite will be sent to any of your buddy \nyou add to this saving plan'),
+                const SizedBox(height: 20),
+                Container(
+                  height: 400,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      border: Border.all(color: Colors.grey, width: 1)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Column(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Target Amount',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w200),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              '#2,500',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w900),
+                            ),
+                            const SizedBox(height: 10),
+                            Container(
+                              height: 20,
+                              width: screenWidth / 2,
+                              decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                  color: Colors.black.withOpacity(0.1)),
+                              child: Center(
+                                child: RichText(
+                                  text: const TextSpan(children: [
+                                    TextSpan(
+                                      text: 'You are saving with',
+                                      style: TextStyle(
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.white),
+                                    ),
+                                    TextSpan(
+                                      text: ' 3 buddie',
+                                      style: TextStyle(
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.blue),
+                                    )
+                                  ]),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 30),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Your target',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        '625,000',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Other buddies target',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        '1,625,000',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              const LinearProgressIndicator(
+                                value: 0.2,
+                                minHeight: 5,
+                                color: Colors.red,
+                                valueColor:
+                                    AlwaysStoppedAnimation<Color>(Colors.white),
+                              ),
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Frequency',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        'Monthly',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        'End Date',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        'Dec 22, 2022',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Start Date',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        'Dec 22, 2022',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        'Interest',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                      Text(
+                                        '2.5% P.A',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Lock Savings?',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w200),
+                                  ),
+                                  Switch(
+                                      inactiveThumbColor:
+                                          Colors.grey.withOpacity(0.6),
+                                      inactiveTrackColor: Colors.white,
+                                      // activeColor: Colors.purple,
+                                      value: _on,
+                                      onChanged: (val) {
+                                        setState(() {
+                                          _on = val;
+                                        });
+                                      }),
+                                ],
+                              ),
+                              Container(
+                                height: 20,
+                                width: screenWidth * 0.9,
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(10),
+                                    ),
+                                    color: Colors.black.withOpacity(0.1)),
+                                child: const Center(
+                                  child: Text(
+                                    'You can\'t withdraw your savings until the set maturity date',
+                                    style: TextStyle(
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.w200,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.add),
+                      SizedBox(width: 5),
+                      Text('Add a buddy', style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),)
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
